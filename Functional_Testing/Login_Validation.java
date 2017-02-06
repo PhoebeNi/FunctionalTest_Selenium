047d0fc9d7bce0f478aaf39e160f4ce13648e149
PhoebeNi <phoebeni0620@gmail.com>
2017-02-05 23:31:26 -0500

    Add Functional_Testing

---
+++ Functional_Testing/Login_Validation.java
@@ -0,0 +1,67 @@
+package Functional_Testing;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+
+public class Login_Validation {
+	private WebDriver driver;
+
+	@Test(dataProvider = "LoginInfo")
+	public void Login(String id, String psw) {
+		driver.findElement(By.name("username")).clear();
+		driver.findElement(By.name("username")).sendKeys(id);
+		driver.findElement(By.name("password")).clear();
+		driver.findElement(By.name("password")).sendKeys(psw);
+		driver.findElement(By.xpath("//*[@id=\"loginfrm\"]/div[4]/button")).click();		
+		try {
+			driver.findElement(By.xpath("//*[@id=\"loginfrm\"]/div[1]/div"));
+			System.out.println("Test Passed!");
+		}
+		catch (NoSuchElementException element){
+			String ActualTitle = driver.getTitle();
+			System.out.println(ActualTitle);
+			if (Util.ExpectedTitlte.equals(ActualTitle)){
+				System.out.println("Test Passed!");
+			}
+			else {
+				System.out.println("Test Failed!");
+			}
+
+		}
+	}
+
+	@BeforeMethod
+	public void beforeMethod() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", Util.ChromeDrider);
+		driver = new ChromeDriver();
+		driver.get(Util.BaseUrl);
+		driver.manage().timeouts().implicitlyWait(Util.WaitTime, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("/html/body/div[2]/div/div/div[2]/ul[2]/li[2]/a")).click();
+		driver.manage().timeouts().implicitlyWait(Util.WaitTime, TimeUnit.SECONDS);
+		driver.findElement(By.partialLinkText("Login")).click();
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		driver.quit();
+	}
+
+
+	@DataProvider
+	public Object[][] LoginInfo() {
+		return new Object[][] {
+			new Object[] { Util.Valid_id, Util.Valid_psw },
+			new Object[] { Util.Valid_id, Util.Invalid_psw },
+			new Object[] { Util.Invalid_id, Util.Valid_psw },
+			new Object[] { Util.Invalid_id, Util.Invalid_psw }
+		};
+	}
+}
---
+++ Functional_Testing/Util.java
@@ -0,0 +1,23 @@
+package Functional_Testing;
+
+public class Util {
+
+	// Chrome Driver Path
+	public static final String ChromeDrider = "E://eclipse//chromedriver.exe";
+	
+	// Waiting Time
+	public static final Integer WaitTime = 30;
+	
+	// Web Address
+	public static final String BaseUrl = "http://phptravels.net/en";
+	
+	// Login Information
+	public static final String Valid_id = "user@phptravels.com";
+	public static final String Valid_psw = "demouser";
+	public static final String Invalid_id = "user1@phptravels.com";
+	public static final String Invalid_psw = "demouser1";
+	
+	// Validation Information
+	public static final String ExpectedTitlte = "My Account";
+	
+}
